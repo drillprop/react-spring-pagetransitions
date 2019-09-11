691618ee6039bb158ea85c90e9349614a372cef7
drillprops <drylbartosz@gmail.com>
2019-09-11 18:17:31 +0200

    add Montserrat as font, set font weight to bold

--- src/components/GlobalStyle.ts
+++ src/components/GlobalStyle.ts
@@ -1,7 +1,10 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css?family=Montserrat:900&display=swap');
   body {
+    font-family: 'Montserrat', sans-serif;
+    font-weight: bold;
     margin: 0
   }
 `;
