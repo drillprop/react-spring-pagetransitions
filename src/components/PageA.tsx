1872c43d30cc3aa5277ca36c67e8d0b63f1dd4ab
drillprops <drylbartosz@gmail.com>
2019-09-11 18:20:58 +0200

    add links on PageA and PageB

--- src/components/PageA.tsx
+++ src/components/PageA.tsx
@@ -1,7 +1,14 @@
 import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: inherit;
+`;
 
 const PageA: FC = () => {
-  return <div>Page A</div>;
+  return <StyledLink to='/b'>B</StyledLink>;
 };
 
 export default PageA;
