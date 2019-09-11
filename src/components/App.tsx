614e14373e000d8f03068df40d5e42d65a8b08c4
drillprops <drylbartosz@gmail.com>
2019-09-11 18:18:48 +0200

    remove navigation, create Wrapper

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,48 +1,29 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import PageA from './PageA';
 import PageB from './PageB';
 import styled from 'styled-components';
 import GlobalStyle from './GlobalStyle';
 
-const StyledNavigation = styled.nav`
-  width: 100%;
-  ul {
-    display: flex;
-    justify-content: center;
-    li {
-      text-transform: uppercase;
-      margin-left: 50px;
-      list-style: none;
-    }
-  }
-`;
-
-const StyledLink = styled(Link)`
-  text-decoration: none;
-  color: inherit;
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  font-size: 90px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const App: React.FC = () => {
   return (
     <>
       <GlobalStyle />
-      <Router>
-        <StyledNavigation>
-          <ul>
-            <StyledLink to='/'>
-              <li>Page A</li>
-            </StyledLink>
-            <StyledLink to='/b'>
-              <li>Page B</li>
-            </StyledLink>
-          </ul>
-        </StyledNavigation>
-        <>
+      <Wrapper>
+        <Router>
           <Route exact path='/' component={PageA} />
           <Route exact path='/b' component={PageB} />
-        </>
-      </Router>
+        </Router>
+      </Wrapper>
     </>
   );
 };
