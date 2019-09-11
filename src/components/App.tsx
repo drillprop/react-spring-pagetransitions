1d5aa907e55f6792915714f6f6688f01634959f3
drillprops <drylbartosz@gmail.com>
2019-09-11 14:48:23 +0200

    create GlobalStyle and App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,24 +2,45 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import PageA from './PageA';
 import PageB from './PageB';
+import styled from 'styled-components';
+import GlobalStyle from './GlobalStyle';
+
+const StyledNavigation = styled.nav`
+  width: 100%;
+  ul {
+    display: flex;
+    justify-content: center;
+    li {
+      text-transform: uppercase;
+      margin-left: 50px;
+      list-style: none;
+    }
+  }
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: inherit;
+`;
 
 const App: React.FC = () => {
   return (
     <>
+      <GlobalStyle />
       <Router>
-        <nav>
+        <StyledNavigation>
           <ul>
-            <Link to='/'>
+            <StyledLink to='/'>
               <li>Page A</li>
-            </Link>
-            <Link to='/b'>
+            </StyledLink>
+            <StyledLink to='/b'>
               <li>Page B</li>
-            </Link>
+            </StyledLink>
           </ul>
-        </nav>
+        </StyledNavigation>
         <>
-          <Route exact path='/' component={PageA}></Route>
-          <Route exact path='/b' component={PageB}></Route>
+          <Route exact path='/' component={PageA} />
+          <Route exact path='/b' component={PageB} />
         </>
       </Router>
     </>
